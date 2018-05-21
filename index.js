4856249324205265626b2ca7254f9c6e2b076e00
Andrew Woodward <ebenwoodward@icloud.com>
2018-05-21 15:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
+  return function () {
     return "FUNKY!";
-  
+  };
 };
 
 
-var theFunk =  funkyFunction()
+var theFunk =  funkyFunction()()
